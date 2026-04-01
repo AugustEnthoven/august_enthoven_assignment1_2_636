df3d5f2e8a30c1409919b14c432ae03804cb309c
August <qutn11138947@gmail.com>
2026-04-01 18:09:38 +1000

    Creating a model for "task"

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const router = express.Router();
 const { getTasks, addTask, updateTask, deleteTask } = require('../controllers/taskController');
-const protect = require('../middleware/authMiddleware'); // This ensures the user is logged in
+const { protect } = require('../middleware/authMiddleware'); // This ensures the user is logged in
+const router = express.Router();
+
+
+console.log("Is getTasks a function?", typeof getTasks);
+console.log("Is protect a function?", typeof protect);
 
 // All routes here will use the "protect" middleware to make sure req.user exists
 router.get('/', protect, getTasks);
