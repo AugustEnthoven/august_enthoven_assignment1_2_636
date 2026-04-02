51b377fb6e07675cc1339097c3fe732b5df626df
August <qutn11138947@gmail.com>
2026-04-02 17:09:39 +1000

    Improving the input box visuals

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -5,7 +5,8 @@ const Search = () => {
         <div>
             <label>
                 <input name = "query"
-                               className="w-full mb-4 p-2 border rounded"
+                       className="w-full mb-4 p-2 border rounded"
+                       placeholder = "Enter your search"
                         />
             </label>
             <button className="mr-2 bg-yellow-500 text-white px-4 py-2 rounded">
