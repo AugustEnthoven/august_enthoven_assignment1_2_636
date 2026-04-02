e2d38fe2453a6f11045fe0a6a4ab2817ee32133f
August <qutn11138947@gmail.com>
2026-04-02 18:23:05 +1000

    Adding a basic home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Tasks from './pages/Tasks';
 import Search from './pages/Search';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/tasks" element={<Tasks />} />
         <Route path ="/search" element={<Search />} />
+        <Route path ="/home" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Products from '../components/ProductsList';
+
+const Home = () => {
+    return (
+            <Products></Products>
+    );
+};
+
+export default Home;
